f016543e5eeee646408f792a81b7df2e83110e23
t.wiaderek <t.wiaderek@gmail.com>
2022-01-18 12:17:30 +0100

    settings.py updated with STATIC_ROOT

--- ni_project/settings.py
+++ ni_project/settings.py
@@ -119,8 +119,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
