1c1813075cc8c8f3da2cb1e84db8152941736cf9
t.wiaderek <t.wiaderek@gmail.com>
2022-01-18 14:41:06 +0100

    settings.py updated with SECRET_KEY and DEBUG

--- ni_project/settings.py
+++ ni_project/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yw!-o4l^i7*y^&b#^ksiftdns4qqtl5$1a3oe!+!&t%p9d2^%('
+SECRET_KEY = os.environ.get('NI_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nothinginclusive.herokuapp.com']
 
 
 # Application definition
